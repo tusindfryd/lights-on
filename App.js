a4d83c04dabc713c25d1dc2b2f4aa6ec8fb09edf
tusindfryd <dominikagoral99@gmail.com>
2020-08-13 19:48:18 +0200

    add nicer UI + an info tab

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { Image, Text, View, TouchableOpacity, Dimensions, Alert } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Image, Text, View, TouchableOpacity, Dimensions, Alert } from 'react-native';
 import { Camera } from 'expo-camera';
-import { Ionicons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
 import * as MediaLibrary from 'expo-media-library';
 import * as Permissions from 'expo-permissions';
 import * as Sharing from 'expo-sharing';
 import * as ImageManipulator from "expo-image-manipulator";
+import * as FileSystem from 'expo-file-system';
+import * as WebBrowser from 'expo-web-browser';
+// import * as Brightness from 'expo-brightness';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [colorIndex, setColorIndex] = useState(0);
   const [photoUri, setPhotoUri] = useState("");
-  let camera;
+  const [showInfo, setshowInfo] = useState(false);
+  const cameraRef = useRef(null);
+
+  const height = Dimensions.get('screen').height;
+  const width = Dimensions.get('screen').width;
+  const previewScale = 0.2;
+  const photoScale = 0.8;
 
   const backgroundColors = [
-    "blue", "red", "green", "yellow", "white"
-  ]
+    "#ff0000", "#ffbf00", "#85a300", "#36cc00", "#00cc9c", "#000dff", "#8800ff", "#bb00ff", "#ff0077"
+  ];
+
+  const styles = StyleSheet.create({
+    preview: {
+      top: (height - (16 / 9 * width * previewScale)) / 2,
+      left: (width - width * previewScale) / 2,
+      width: width * previewScale,
+      height: 16 / 9 * width * previewScale
+    },
+    container: {
+      position: "absolute",
+      borderColor: "white",
+      borderWidth: 1.5,
+      top: (height - (16 / 9 * width * photoScale)) / 2,
+      left: (width - width * photoScale) / 2,
+      width: width * photoScale,
+      height: 16 / 9 * width * photoScale,
+      display: "flex",
+    },
+    icons: {
+      position: "absolute",
+      width: width,
+      bottom: (height - 16 - ((height - (16 / 9 * width * photoScale)) / 2 + 16 / 9 * width * photoScale)) / 2,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center"
+    },
+    info: {
+      color: "white",
+      paddingHorizontal: 30,
+      textAlign: "justify",
+      lineHeight: 25
+    }
+  });
 
   useEffect(() => {
     (async () => {
@@ -24,9 +66,19 @@ export default function App() {
     })();
   }, []);
 
+  // maybe this will work one day but now is not that day
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Brightness.requestPermissionsAsync();
+  //     if (status === 'granted') {
+  //       Brightness.setSystemBrightnessAsync(1);
+  //     }
+  //   })();
+  // }, []);
+
   let takePicture = async () => {
-    if (camera) {
-      let photo = await camera.takePictureAsync();
+    if (cameraRef.current) {
+      let photo = await cameraRef.current.takePictureAsync();
       photo = await ImageManipulator.manipulateAsync(photo.uri,
         [{ flip: ImageManipulator.FlipType.Horizontal }],
         { compress: 1, format: ImageManipulator.SaveFormat.JPEG })
@@ -34,17 +86,35 @@ export default function App() {
     }
   }
 
+  let deletePicture = () => {
+    Alert.alert(
+      "Delete photo",
+      "The photo will be deleted.",
+      [
+        { text: "Cancel", style: "cancel" },
+        {
+          text: "Okay", onPress: () => {
+            FileSystem.deleteAsync(photoUri);
+            setPhotoUri("");
+          }
+        }
+      ],
+      { cancelable: true }
+    );
+  }
+
   let savePicture = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     if (status === 'granted') {
-      MediaLibrary.saveToLibraryAsync(photoUri);
+      await MediaLibrary.saveToLibraryAsync(photoUri);
+      await FileSystem.deleteAsync(photoUri);
       Alert.alert(
         "Saved",
         "The photo was saved to the Camera Roll.",
         [
           { text: "Okay", onPress: () => setPhotoUri("") }
         ],
-        { cancelable: false }
+        { cancelable: true }
       );
     }
     else {
@@ -56,73 +126,143 @@ export default function App() {
     await Sharing.shareAsync(photoUri);
   };
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  if (photoUri) {
-    return (
-      <View style={{
-        height: Dimensions.get('screen').height,
-        backgroundColor: backgroundColors[colorIndex % backgroundColors.length]
-      }}>
-        <Image source={{ uri: photoUri }}
-          style={{
-            top: (Dimensions.get('screen').height - (16 / 9 * Dimensions.get('screen').width * 0.8)) / 2,
-            left: (Dimensions.get('screen').width - Dimensions.get('screen').width * 0.8) / 2,
-            width: Dimensions.get('screen').width * 0.8,
-            height: 16 / 9 * Dimensions.get('screen').width * 0.8
-          }} />
+  let openTwitter = async () => {
+    await WebBrowser.openBrowserAsync('https://twitter.com/intent/tweet?url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dio.lightson&text=');
+  };
 
-        <View style={{
-          position: "absolute",
-          width: Dimensions.get('screen').width,
-          bottom: 50,
-          display: "flex", flexDirection: "row", justifyContent: "center"
-        }}>
-          <Ionicons name="ios-save" size={32} color="white" accessibilityLabel="Save" style={{ paddingHorizontal: 30 }}
-            onPress={() => savePicture()}
-          />
-          <Ionicons name="ios-trash" size={32} color="white" accessibilityLabel="Delete" style={{ paddingHorizontal: 30 }}
-            onPress={() => setPhotoUri("")}
-          />
-          <Ionicons name="ios-share" size={32} color="white" accessibilityLabel="Share" style={{ paddingHorizontal: 30 }}
-            onPress={() => sharePicture()}
-          />
-        </View>
-      </View>
-    )
-  }
+  let openGithub = async () => {
+    await WebBrowser.openBrowserAsync('https://github.com/tusindfryd/lights-on');
+  };
+
   return (
-    <TouchableOpacity style={{
-      width: Dimensions.get('screen').width,
-      height: Dimensions.get('screen').height,
-      backgroundColor: backgroundColors[colorIndex % backgroundColors.length]
-    }} onPress={() => setColorIndex(colorIndex + 1)}>
+    <TouchableOpacity
+      style={{
+        width: width,
+        height: height,
+        backgroundColor: backgroundColors[colorIndex % backgroundColors.length]
+      }}
+      onPress={() => photoUri == "" ? setColorIndex(colorIndex + 1) : null}
+    >
+      {hasPermission === false &&
+        <View style={styles.container}>
+          <Text style={{ flex: 1, textAlignVertical: "center", textAlign: "center", color: "white" }}>
+            Camera permissions not granted.
+          </Text>
+        </View>
+      }
 
-      <Camera
-        ref={ref => (camera = ref)}
+      <EvilIcons name="eye"
+        onPress={() => showInfo ? setshowInfo(false) : setshowInfo(true)}
+        size={32}
+        color="white"
         style={{
-          top: (Dimensions.get('screen').height - (16 / 9 * Dimensions.get('screen').width * 0.2)) / 2,
-          left: (Dimensions.get('screen').width - Dimensions.get('screen').width * 0.2) / 2,
-          width: Dimensions.get('screen').width * 0.2,
-          height: 16 / 9 * Dimensions.get('screen').width * 0.2
+          position: "absolute",
+          top: 50,
+          right: 30
         }}
-        ratio="16:9"
-        type={Camera.Constants.Type.front}
-        flash={Camera.Constants.FlashMode.off}
-        whiteBalance={Camera.Constants.WhiteBalance.fluorescent}
-      >
-        <TouchableOpacity style={{
-          width: Dimensions.get('screen').width * 0.2,
-          height: 16 / 9 * Dimensions.get('screen').width * 0.2
-        }}
-          onPress={() => takePicture()}>
-        </TouchableOpacity>
-      </Camera>
+      />
 
+      {showInfo &&
+        <TouchableOpacity
+          style={[styles.container, { zIndex: 1, paddingVertical: 30 }]}
+          onPress={() => setshowInfo(false)}
+        >
+          <Text
+            style={[styles.info, { fontWeight: "bold", textAlign: "center", paddingTop: 30, flex: 1 }]}
+          >
+            Lights On{'\n'}
+          </Text>
+
+          <Text style={[styles.info, { flex: 2 }]}>
+            Tap the <Text style={{ fontWeight: "bold" }}>background </Text>
+            to change the <Text style={{ fontWeight: "bold" }}>light color</Text>.{'\n'}
+            Tap the <Text style={{ fontWeight: "bold" }}>camera preview </Text>
+            to <Text style={{ fontWeight: "bold" }}>take a photo</Text>.
+          </Text>
+
+          <Text style={[styles.info, { flex: 2, fontWeight: "bold" }]}>
+            Works best with brightness set to maximum.
+          </Text>
+
+          <View
+            style={[styles.info, { flex: 1, display: "flex", flexDirection: "row", justifyContent: "center" }]}
+          >
+            <EvilIcons
+              name="sc-github"
+              size={32}
+              color="white"
+              style={{ paddingHorizontal: 30 }}
+              onPress={() => openGithub()}
+            />
+
+            <EvilIcons
+              name="sc-twitter"
+              size={32}
+              color="white"
+              style={{ paddingHorizontal: 30 }}
+              onPress={() => openTwitter()}
+            />
+          </View>
+
+          <Text style={[styles.info, { flex: 1, textAlign: "center" }]}>2020</Text>
+
+        </TouchableOpacity>
+      }
+
+      {hasPermission === true && photoUri == "" && !showInfo &&
+        <Camera
+          ref={cameraRef}
+          style={styles.preview}
+          ratio="16:9"
+          type={Camera.Constants.Type.front}
+          flash={Camera.Constants.FlashMode.off}
+          whiteBalance={Camera.Constants.WhiteBalance.sunny}
+        >
+          <TouchableOpacity
+            style={{
+              borderColor: "white",
+              borderWidth: 1.5,
+              width: width * previewScale,
+              height: 16 / 9 * width * previewScale
+            }}
+            onPress={() => takePicture()}
+          />
+        </Camera>
+      }
+
+      {photoUri !== "" && !showInfo &&
+        <>
+          <Image source={{ uri: photoUri }}
+            style={styles.container}
+          />
+          <View style={styles.icons}>
+            <EvilIcons
+              name="trash"
+              size={32}
+              color="white"
+              accessibilityLabel="Delete"
+              style={{ paddingHorizontal: 30 }}
+              onPress={() => deletePicture()}
+            />
+            <EvilIcons
+              name="archive"
+              size={32}
+              color="white"
+              accessibilityLabel="Save"
+              style={{ paddingHorizontal: 30 }}
+              onPress={() => savePicture()}
+            />
+            <EvilIcons
+              name="share-apple"
+              size={32}
+              color="white"
+              accessibilityLabel="Share"
+              style={{ paddingHorizontal: 30 }}
+              onPress={() => sharePicture()}
+            />
+          </View>
+        </>
+      }
     </TouchableOpacity>
   );
 }
